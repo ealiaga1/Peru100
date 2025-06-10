36f7f35e1239cf33be2056e57b6072ee71c4aca8
Raptel <49075349+ealiaga1@users.noreply.github.com>
2025-06-10 18:21:17 -0500

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,7 +1,1 @@
-<?php
-define('DB_HOST', 'localhost'); // O la IP de tu servidor de BD
-define('DB_NAME', 'u210964406_resto2025'); // El nombre de tu base de datos
-define('DB_USER', 'u210964406_uresto2025');   // Tu usuario de la base de datos
-define('DB_PASS', '1324MMa1240$'); // La contraseña de tu usuario de la base de datos
-define('DB_CHARSET', 'utf8mb4');
-?>
+
